1eac43b50ee7cf28b03ea6afc3b0c06bb2f0c388
Kirill Neevin <neevin@yandex-team.ru>
2023-02-20 09:51:51 +0300

    fixed issue

--- lab1/src/test/java/softwaretesting/lab1/test1/TestTgFunc.java
+++ lab1/src/test/java/softwaretesting/lab1/test1/TestTgFunc.java
@@ -1,10 +1,10 @@
 package softwaretesting.lab1.test1;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 import softwaretesting.lab1.task1.TgFunc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TestTgFunc {
@@ -13,7 +13,7 @@ public class TestTgFunc {
     @ParameterizedTest
     @ValueSource(doubles = { Math.PI/6, Math.PI/4, Math.PI/3, 0, Math.PI/6, Math.PI/4, Math.PI/3 })
     public void baseLogin(double value) {
-        Assertions.assertEquals(Math.tan(value), TgFunc.tg(value), eps);
+        assertEquals(Math.tan(value), TgFunc.tg(value), eps);
     }
 
     @ParameterizedTest
@@ -27,12 +27,12 @@ public class TestTgFunc {
             Double.POSITIVE_INFINITY
     })
     public void undefined(double value) {
-        Assertions.assertEquals(Double.NaN, TgFunc.tg(value), eps);
+        assertEquals(Double.NaN, TgFunc.tg(value), eps);
     }
 
     @ParameterizedTest
     @ValueSource(doubles = { Math.PI/2-0.1, Math.PI/2-0.01 })
     public void breakPoint(double value) {
-        Assertions.assertEquals(Math.tan(value), TgFunc.tg(value), eps*190);
+        assertEquals(Math.tan(value), TgFunc.tg(value), eps*190);
     }
 }
